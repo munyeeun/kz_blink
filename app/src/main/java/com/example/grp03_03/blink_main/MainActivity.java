74aaa563c825dc0192b7318d07e80df81eb354b3
munyeeun <munyeeun97@gmail.com>
2018-09-15 13:57:49 +0900

    login page

---
+++ app/src/main/java/com/example/grp03_03/blink_main/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.grp03_03.blink_main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
